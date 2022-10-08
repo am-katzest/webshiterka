bcdae2f4d9cd6e8a65c4e7435cb5b20ac5b4a9d9
bestestest kitteh <236551@edu.p.lodz.pl>
2022-10-08 17:20:53 +0200

    some changes

--- src/webshiterka/core.clj
+++ src/webshiterka/core.clj
@@ -10,34 +10,38 @@
        (mapv c/components-argb)
        (partition (m/width image))))
 
-(def space "<div id=\"hacky\"/>")
-
 (defn htmlize-color [colors] (apply  format "#%02x%02x%02x%02x" colors))
 
 (defn make-pixel [[count colors]]
   (let [cstr (htmlize-color colors)
         width (if (= count 1) ""
                   (format "width:%dem;" count))]
-    (format "<div id=\"square\" style=\"background:%s;%s\"></div>" cstr width)))
+    (format "<div id=\"pixel\" style=\"background:%s;%s\"></div>" cstr width)))
 
 (defn transform-pixel-array-into-html [arr]
-  (apply str (mapcat (fn [row] (concat [space] (map make-pixel row))) arr)))
+  (apply str (mapcat (fn [row] (concat ["<div id=\"newline\"/>"] (map make-pixel row))) arr)))
 
 (defn group-pixels [row]
   (->> row
        (partition-by identity)
        (map (juxt count first))))
 
+(defn image2html [filename]
+  (->> filename
+       java.io.File.
+       m/load-image
+       split-into-pixels
+       (map group-pixels)
+       transform-pixel-array-into-html))
+
+(defn mod-template [html [placeholder image]]
+  (string/replace html placeholder
+                  (image2html image)))
+
 (defn -main
-  [template in]
-  (let [html (slurp template)
-        insert-into-template
-        #(string/replace html #"%%%" %1)]
-    (->> in
-         java.io.File.
-         m/load-image
-         split-into-pixels
-         (map group-pixels)
-         transform-pixel-array-into-html
-         insert-into-template
+  [template & replacements]
+  (let [html (slurp template)]
+    (->> replacements
+         (partition 2)
+         (reduce mod-template html)
          println)))
