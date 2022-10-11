518f73d7a4ac9e20bed863b646a30cb59d922adf
bestestest kitteh <236551@edu.p.lodz.pl>
2022-10-11 10:02:52 +0200

    + bin

--- src/main/todoot/core.cljs
+++ src/main/todoot/core.cljs
@@ -1,12 +1,19 @@
-(ns  todoot.core)
+(ns  todoot.core
+  (:require [ajax.core :as ax]))
+
+(def api-key "$2b$10$WeANikw9XvZVJZENB5lcOedfsqnvEEtJtPFzotoM7i3UhU/eYuP1S")
+(def bin "634471970e6a79321e23f901")
+(defn get-todos-from-api [cb]
+  (ax/GET (str "https://api.jsonbin.io/v3/b/" bin)
+    {:headers {:X-Access-Key api-key}
+     :handler #(cb (get % "record"))
+     :error-handler #(js/alert %)}))
 
 (defrecord todo [title description place dueDate])
 
-(def todos (atom #{}))
+(defonce todos (atom #{}))
 
 (defn append-todo! [new] (swap! todos conj new))
-(append-todo! (->todo "Learn JS" "Create a demo application for my TODO's" "445" (js/Date. 2019 10 16)))
-(append-todo! (->todo "Lecture Test" "Quick test from the first three lectures" "F6" (js/Date. 2019 10 17)))
 (defn get-html-value [name] (.-value (.getElementById js/document name)))
 (defn read-todo []
   (let [[title desc place date]
@@ -55,21 +62,19 @@
 (defn deleter [item]
   (swap! todos disj item)
   (update-todo-list))
-(defn save [] (->> @todos
-                   clj->js
-                   (.stringify js/JSON)
-                   (.setItem js/window.localStorage "todos")))
+
+(defn get-todos [] (->> @todos
+                        clj->js
+                        (.stringify js/JSON)))
 
 (defn keywordize [x]
   (into {} (map (fn [[k v]] [(keyword k) v]) x)))
 
-(defn load [] (when-let [read (.getItem js/window.localStorage  "todos")]
-                (->> read
-                     (.parse js/JSON)
-                     js->clj
-                     (map keywordize)
-                     (into #{})
-                     (reset! todos))
-                (update-todo-list)))
-
-(defn init [] (.addEventListener js/window "load" update-todo-list))
+(defn load-todos! [new]
+  (->> new
+       js->clj
+       (map keywordize)
+       (into #{})
+       (reset! todos)))
+(defn init [] (get-todos-from-api #(do (load-todos! %) (update-todo-list)))
+  (.addEventListener js/window "load" update-todo-list))
