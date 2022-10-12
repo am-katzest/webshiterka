c4bb51f142673c6e4f8eadc50b8041ce20abd8dc
bestestest kitteh <236551@edu.p.lodz.pl>
2022-10-12 10:30:13 +0200

    add sending

--- src/main/todoot/bin.cljs
+++ src/main/todoot/bin.cljs
@@ -1,19 +1,19 @@
 (ns todoot.bin
   (:require [ajax.core :as ax]))
 
-(def api-key "$2b$10$WeANikw9XvZVJZENB5lcOedfsqnvEEtJtPFzotoM7i3UhU/eYuP1S")
+(def api-key "$2b$10$aTUTTwvgslmXXpwWAxJ4j.hyTIPO4yMsSeE6vtZzRhBafUjQXKMA6")
 
-(def bin "634471970e6a79321e23f901")
+(def bin-url (str "https://api.jsonbin.io/v3/b/" "634471970e6a79321e23f901"))
 
 (defn get-todos-from-api [cb]
-  (ax/GET (str "https://api.jsonbin.io/v3/b/" bin)
-    {:headers {:X-Access-Key api-key}
+  (ax/GET bin-url
+    {:headers {:X-Master-Key api-key}
      :handler #(cb (get % "record"))
      :error-handler #(js/alert %)}))
 
-(defn send-todos-to-api [json]
-  (comment (ax/POST (str "https://api.jsonbin.io/v3/b/" bin)
-             {:headers {:X-Access-Key api-key}
-              :contentType "application/json"
-              :data json
-              :error-handler #(js/alert %)})))
+(defn send-todos-to-api [data]
+  (ax/PUT bin-url
+    {:headers {:X-Master-Key api-key
+               :Content-Type "application/json"}
+     :body data
+     :error-handler #(js/alert %)}))
